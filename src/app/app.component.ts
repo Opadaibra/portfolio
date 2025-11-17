a198a11cb5dbb26a7e7501bd9290904fa703fcba
o.ebraheem <o.ebraheem@smartsoft-sy.net>
2025-11-17 21:33:38 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,30 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  isMenuOpen = false;
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+    this.isMenuOpen = false;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  downloadCV() {
+    // يمكنك إضافة منطق تحميل السيرة الذاتية هنا
+    const link = document.createElement('a');
+    link.href = 'path-to-your-cv.pdf';
+    link.download = 'Obada_Ibrahim_CV.pdf';
+    link.click();
+  }
 }
